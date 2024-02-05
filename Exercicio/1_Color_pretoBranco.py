87adf1b784771238a10a2be296c4fe158dd99085
Thomas Henrique <thomasnhenrique@gmail.com>
2024-02-05 20:22:49 -0300

    1º Exercicio
    Primeiro exercicio feito em aula.

---
+++ Exercicio/1_Color_pretoBranco.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.insert(0, 'IMGS')
+sys.path.insert(0, 'LIBS')
+
+import math
+from tkinter import *
+from graphics import *
+
+win = GraphWin("eSTUDO de cores", 640, 720)
+im = PhotoImage(file ='Sonic2 (1).png')
+im2 = Image(Point(300, 350), 'Sonic2 (1).png')
+
+for i in range (0, 150):
+    for j in range (0, 197):
+        r, g, b = im.get(i, j)
+        px = int(i  + 50)
+        py = int(j + 50)
+
+        media = int ((r+g+b)/3)
+
+        if(media>=127):
+            media=255
+        else:
+            media=0
+        
+        colorval = "#%02x%02x%02x" % (media, media, media)
+        
+        win.create_rectangle(
+            px, py, px, py, width = 0, fill=(colorval))
+
+im2.draw(win)
+win.mainloop()
+win.close()
